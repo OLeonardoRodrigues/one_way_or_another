bc79021c8b57055b1ab91161d725ce2f892196ef
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2020-09-15 10:31:04 -0300

    Format Files

--- test/one_way_or_another_test.exs
+++ test/one_way_or_another_test.exs
@@ -6,15 +6,19 @@ defmodule OneWayOrAnotherTest do
   test "Binary Search the first item" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0) == {true, 0}
   end
+
   test "Binary Search the middle item" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5) == {true, 5}
   end
+
   test "Binary Search the last item" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10) == {true, 10}
   end
+
   test "Binary Search the second item" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3) == {true, 3}
   end
+
   test "Binary Search the second to last item" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9) == {true, 9}
   end
@@ -23,6 +27,7 @@ defmodule OneWayOrAnotherTest do
   test "Binary Search one number bellow the first one" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1) == {false, -1}
   end
+
   test "Binary Search one number above the last one" do
     assert OneWayOrAnother.binary([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11) == {false, -1}
   end
@@ -31,15 +36,19 @@ defmodule OneWayOrAnotherTest do
   test "Linear Search the first item" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0) == {true, 0}
   end
+
   test "Linear Search the middle item" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5) == {true, 5}
   end
+
   test "Linear Search the last item" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10) == {true, 10}
   end
+
   test "Linear Search the second item" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3) == {true, 3}
   end
+
   test "Linear Search the second to last item" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9) == {true, 9}
   end
@@ -48,6 +57,7 @@ defmodule OneWayOrAnotherTest do
   test "Linear Search one number bellow the first one" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1) == {false, -1}
   end
+
   test "Linear Search one number above the last one" do
     assert OneWayOrAnother.linear([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11) == {false, -1}
   end
